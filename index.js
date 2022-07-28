43bfabda0215e1768a8bb005ce3e618f6f898bc5
Asad <asad200ana@gmail.com>
2022-07-28 19:24:43 +0530

    players

--- index.js
+++ index.js
@@ -16,6 +16,11 @@ class Sprite {
     c.fillStyle = "red";
     c.fillRect(this.position.x, this.position.y, 50, 150);
   }
+
+  update() {
+    this.draw();
+    this.position.y += 10;
+  }
 }
 
 const player = new Sprite({
@@ -29,12 +34,10 @@ const player = new Sprite({
   },
 });
 
-player.draw();
-
 const enemy = new Sprite({
   position: {
-    x: 0,
-    y: 0,
+    x: 400,
+    y: 100,
   },
   velocity: {
     x: 0,
@@ -42,13 +45,13 @@ const enemy = new Sprite({
   },
 });
 
-enemy.draw();
-
 console.log(player);
 
 function animate() {
   window.requestAnimationFrame(animate);
   console.log("go");
+  player.update();
+  enemy.update();
 }
 
 animate();
